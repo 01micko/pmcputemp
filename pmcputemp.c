ce6965822537b6922f2569bcfd9bdf82bd0935d0
01micko <01micko@gmail.com>
2015-05-06 16:48:00 +1000

    pmcputemp.c: fix gtk3 support add help menu if configured add lm_sensors menu if configured bugfix for not loading icon on first run add module parameter on first run add theme parameters, light and dark

--- pmcputemp.c
+++ pmcputemp.c
@@ -30,6 +30,9 @@
 #include <libintl.h>
 #include <locale.h>
 #include "about.h"
+#ifdef HAVE_SENSORS
+	#include "lm.h"
+#endif /* HAVE_SENSORS */
 
 #define _GNU_SOURCE
 #define ICON ".config/pmcputemp/temp.png"
@@ -42,6 +45,8 @@ char temp_icon[128];
 static char configdir[256];
 static char conf[256];
 char s[4];
+char *module = NULL;
+char command[40];
 /* poor man's degree symbol for cairo's poor font handling */
 const char deg[2] = "o"; 
 char line[512];
@@ -52,6 +57,8 @@ gchar *tool_tip;
 int x_procs;
 GdkPixbuf *temp_pixbuf;
 GtkStatusIcon *tray_icon;
+char style;
+int first_run = 1;
 
 unsigned int interval = 5000; /*update interval in milliseconds*/ 
 
@@ -60,10 +67,16 @@ FILE *ft;
 const char *home;
 GError *gerror = NULL;
 
-
-int mk_conf() {
+int mk_conf(char *mod) {
 	/* script to setup system temperature location */
-	int ret_val = system("sh pmcputemp.sh");
+	first_run = 0;
+	if (mod != NULL) {
+		printf("load module : %s\n", mod);
+		command[40] = sprintf(command, "sh pmcputemp.sh %s", module);
+	} else {
+		command[40] = sprintf(command, "sh pmcputemp.sh");
+	}
+	int ret_val = system(command);
 	return ret_val;
 }
 
@@ -82,7 +95,7 @@ int cpu_temp() {
 		}
 		fp = fopen(conf, "r");
 		if ( fp == NULL ) {
-			ret_try = mk_conf();
+			ret_try = mk_conf(module);
 			if (ret_try != 0) {
 				fprintf(stderr, _("Unable to create configuration file.\n"));
 				show_about(1);
@@ -114,6 +127,7 @@ int cpu_temp() {
 		int success_temp = fscanf(ft, "%d", &temp_val);
 		if (success_temp < 1) {
 			fprintf(stderr,_("Failed to read temperature, giving up.\n"));
+			fclose(ft);
 			show_about(1);
 			exit(1);
 		}
@@ -129,35 +143,58 @@ int cpu_temp() {
 }
 
 /* makes the tray icon to be displayed */
-int paint_icon() {
+int paint_icon(char style) {
 	home = getenv("HOME");
-	int temp = cpu_temp();
+	int temp; 
+	if (style == 'x') {
+		temp = 00;
+	} else {
+		temp = cpu_temp();
+	}
 	configdir[256] = snprintf(configdir, 
 						sizeof(configdir),"%s/%s", home, CONFDIR);
 	mkdir(configdir, 0755);
 	temp_icon[128] = snprintf(temp_icon,
 						sizeof(temp_icon),"%s/%s", home, ICON);
 	s[4] = snprintf(s, sizeof(s),"%d", temp);
-	float r1, g1, b1, r2, g2, b2;
-	if (temp < 46 ) {
-		r1 = 0.4; r2 = 0.2;
-		g1 = 0.9; g2 = 0.7;
-		b1 = 0.9; b2 = 0.7;
-	}
-	if ((temp >= 46) && (temp <= 70)) {
-		r1 = 0.4; r2 = 0.4;
-		g1 = 0.9; g2 = 0.7;
-		b1 = 0.4; b2 = 0.4;
-	}
-	if ((temp > 70 ) && (temp <= 80)) {
-		r1 = 0.6; r2 = 0.8;
-		g1 = 0.3; g2 = 0.5;
-		b1 = 0.1; b2 = 0.1;
+	float r1, g1, b1, r2, g2, b2, fr, fg, fb;
+	switch (style) {
+		case 'd': /* dark */
+			r1 = 0.1; r2 = 0.4; fr = 0.9;
+			g1 = 0.1; g2 = 0.4; fg = 0.9;
+			b1 = 0.1; b2 = 0.4; fb = 0.9;
+			break;
+		case 'l': /* light */
+			r1 = 1.0; r2 = 0.7; fr = 0.1;
+			g1 = 1.0; g2 = 0.7; fg = 0.1;
+			b1 = 1.0; b2 = 0.7; fb = 0.1;
+			break;
+		default:
+		if (temp < 46 ) {
+			r1 = 0.4; r2 = 0.2;
+			g1 = 0.9; g2 = 0.7;
+			b1 = 0.9; b2 = 0.7;
+		}
+		if ((temp >= 46) && (temp <= 70)) {
+			r1 = 0.4; r2 = 0.4;
+			g1 = 0.9; g2 = 0.7;
+			b1 = 0.4; b2 = 0.4;
+		}
+		if ((temp > 70 ) && (temp <= 80)) {
+			r1 = 0.6; r2 = 0.8;
+			g1 = 0.3; g2 = 0.5;
+			b1 = 0.1; b2 = 0.1;
+		}
+		
+		fr = 0.1;
+		fg = 0.1;
+		fb = 0.1;
+		break;
 	}
 	if (temp > 80 ) {
 		r1 = 0.9; r2 = 0.7;
-		g1 = 0.7; g2 = 0.4;
-		b1 = 0.7; b2 = 0.4;		
+		g1 = 0.4; g2 = 0.4;
+		b1 = 0.4; b2 = 0.4;		
 	}
 	if (temp > 99) {
 		exit (1);
@@ -190,11 +227,11 @@ int paint_icon() {
 	cairo_fill_preserve (c);
 	cairo_set_font_size(c, 14.0);
 	cairo_move_to(c, 1.0, 17.0);
-	cairo_set_source_rgb(c, 0.0, 0.0, 0.0);
+	cairo_set_source_rgb(c, fr, fg, fb);
 	cairo_show_text(c, s);
 	cairo_set_font_size(c, 8.0);
 	cairo_move_to(c, 18.0, 11.0);
-	cairo_set_source_rgb(c, 0.0, 0.0, 0.0);
+	cairo_set_source_rgb(c, fr, fg, fb);
 	cairo_show_text(c, deg);
 	cairo_surface_write_to_png (cs, temp_icon);
 	cairo_destroy(c);
@@ -257,12 +294,20 @@ gchar *get_tt(int num){
 
 gboolean Update(gpointer ptr) {
 	/* refreshes the icon */
-	paint_icon();
-	temp_pixbuf = gdk_pixbuf_new_from_file(temp_icon,&gerror);
-	gtk_status_icon_set_from_pixbuf(tray_icon,temp_pixbuf);
+	paint_icon(style);
+	if (first_run == 0) {
+		sleep(1);
+		first_run = 1;
+	}
 	x_procs = num_procs();
 	tool_tip = get_tt(x_procs);
+#ifdef HAVE_GTK3
+	gtk_status_icon_set_tooltip_text(tray_icon, tool_tip);
+#else
 	gtk_status_icon_set_tooltip(tray_icon, tool_tip);
+#endif /* HAVE_GTK3 */
+	temp_pixbuf = gdk_pixbuf_new_from_file(temp_icon,&gerror);
+	gtk_status_icon_set_from_pixbuf(tray_icon,temp_pixbuf);
 	g_free(tooltip_out);
 	return 1;
 }
@@ -270,6 +315,26 @@ gboolean Update(gpointer ptr) {
 void  view_popup_menu_about(GtkWidget *w, gpointer dummy) {
 	show_about(0); /* "About - menu */
 }
+
+#ifdef HAVE_SENSORS
+
+void view_sensors(GtkWidget *w, gpointer dummy) {
+	sensor_gui(0, NULL);
+}
+#endif /* HAVE_SENSORS */
+
+#ifdef HAVE_HELP
+
+void view_help(GtkWidget *w, gpointer dummy) {
+	fp = popen("mdview /usr/share/pmcputemp '' '' 'pmcputemp help' &", "r");
+	if (!fp) {
+		fprintf(stderr, "Failed to open help file");
+		return;
+	}
+	pclose(fp);
+}
+#endif /* HAVE_HELP */
+
 
 void  quit(GtkWidget *w, gpointer dummy) {
     gtk_main_quit();
@@ -280,14 +345,46 @@ void tray_icon_on_menu(GtkStatusIcon *status_icon, guint button,guint activate_t
 	GtkWidget *menu, *menuitem, *iconw;
 	menu = gtk_menu_new();
 	
+#ifdef HAVE_HELP
+	menuitem = gtk_image_menu_item_new_with_label(_("Help"));
+	#ifdef HAVE_GTK3
+	iconw = gtk_image_new_from_icon_name("help-contents", GTK_ICON_SIZE_MENU);
+	#else
+	iconw = gtk_image_new_from_stock(GTK_STOCK_HELP, GTK_ICON_SIZE_MENU);
+    #endif /* HAVE_GTK3 */
+    gtk_image_menu_item_set_image(GTK_IMAGE_MENU_ITEM(menuitem), iconw);
+    gtk_menu_shell_append(GTK_MENU_SHELL(menu), menuitem);
+	g_signal_connect(menuitem, "activate", (GCallback) view_help, status_icon);
+#endif /* HAVE_HELP */
+	
+#ifdef HAVE_SENSORS
+	menuitem = gtk_image_menu_item_new_with_label(_("Info"));
+	#ifdef HAVE_GTK3
+	iconw = gtk_image_new_from_icon_name("dialog-information", GTK_ICON_SIZE_MENU);
+	#else
+	iconw = gtk_image_new_from_stock(GTK_STOCK_INFO, GTK_ICON_SIZE_MENU);
+    #endif /* HAVE_GTK3 */
+    gtk_image_menu_item_set_image(GTK_IMAGE_MENU_ITEM(menuitem), iconw);
+    gtk_menu_shell_append(GTK_MENU_SHELL(menu), menuitem);
+	g_signal_connect(menuitem, "activate", (GCallback) view_sensors, status_icon);
+#endif /* HAVE_SENSORS */
+	
 	menuitem = gtk_image_menu_item_new_with_label(_("About"));
+#ifdef HAVE_GTK3
+	iconw = gtk_image_new_from_icon_name("help-about", GTK_ICON_SIZE_MENU);
+#else
 	iconw = gtk_image_new_from_stock(GTK_STOCK_ABOUT, GTK_ICON_SIZE_MENU);
+#endif /* HAVE_GTK3 */
     gtk_image_menu_item_set_image(GTK_IMAGE_MENU_ITEM(menuitem), iconw);
     gtk_menu_shell_append(GTK_MENU_SHELL(menu), menuitem);
 	g_signal_connect(menuitem, "activate", (GCallback) view_popup_menu_about, status_icon);
 	
 	menuitem = gtk_image_menu_item_new_with_label(_("Quit"));
+#ifdef HAVE_GTK3    
+    iconw = gtk_image_new_from_icon_name("application-exit", GTK_ICON_SIZE_MENU);
+#else
     iconw = gtk_image_new_from_stock(GTK_STOCK_QUIT, GTK_ICON_SIZE_MENU);
+#endif /* HAVE_GTK3 */     
     gtk_image_menu_item_set_image(GTK_IMAGE_MENU_ITEM(menuitem), iconw);
     gtk_menu_shell_append(GTK_MENU_SHELL(menu), menuitem);
     g_signal_connect(menuitem, "activate", (GCallback) quit, status_icon);
@@ -297,6 +394,10 @@ void tray_icon_on_menu(GtkStatusIcon *status_icon, guint button,guint activate_t
 }
 
 static GtkStatusIcon *create_tray_icon() {
+	if (first_run == 0) {
+		sleep(1);
+		first_run = 1;
+	}
 	tray_icon = gtk_status_icon_new();
 	g_signal_connect(G_OBJECT(tray_icon), "popup-menu", G_CALLBACK(tray_icon_on_menu), NULL);
 	
@@ -304,8 +405,7 @@ static GtkStatusIcon *create_tray_icon() {
 	temp_icon[128] = snprintf(temp_icon, sizeof(temp_icon),"%s/%s", home, ICON);
         
 	temp_pixbuf = gdk_pixbuf_new_from_file(temp_icon,&gerror);
-	gtk_status_icon_set_from_pixbuf(tray_icon,temp_pixbuf);							   
-	gtk_status_icon_set_tooltip_text(tray_icon, _("CPU temperature"));
+	gtk_status_icon_set_from_pixbuf(tray_icon,temp_pixbuf);
 							   
 	gtk_status_icon_set_visible(tray_icon, TRUE);
 
@@ -316,23 +416,49 @@ int main(int argc, char **argv) {
 	setlocale( LC_ALL, "" ); 
 	bindtextdomain( "pmcputemp", "/usr/share/locale" ); 
 	textdomain( "pmcputemp" );
-	if (argc == 2) { /* only accepts 1 - 10 */
-		interval = atoi(argv[1]); /* returns 0 if garbage is input */
-		if ((interval < 1) || (interval > 10)) {
-		fprintf(stderr,_("Polling interval out of range. Use 1 to 10\n"
-			"Using default 5 seconds\n"));
-			interval = 5;
+	int i;
+	paint_icon('x'); /* paint it early with a dummy value */
+	if (argc < 5) { /* only accepts 1 - 10 and/or 'l' or 'd' */
+		for (i = 1; i < argc; i++) {
+			switch (argv[i][0]) {
+				case 'd':
+				style='d';
+				break;
+				case 'l':
+				style='l';
+				break;
+				default:
+				if (argv[i][1]) { /* test if module specified on cli */
+					if (module != NULL) {
+						fprintf(stderr, 
+							"kernel module %s is already specified\n", module);
+						break;
+					} else {
+						module = argv[i];
+					}
+				} else {
+					interval = atoi(argv[i]);
+					if ((interval < 1) || (interval > 10)) {
+					fprintf(stderr,_("Polling interval out of range. Use 1 to 10\n"
+						"Using default 5 seconds\n"));
+						interval = 5;
+					}
+					interval = interval * 1000; /* millisecs */
+					break;
+				}
+			}
 		}
-		interval = interval * 1000; /* millisecs */
+	} else {
+		fprintf(stderr,"Too many arguments, loading defaults\n");
 	}
+	
 	gtk_init(&argc, &argv);
-	paint_icon();
+	paint_icon(style);
 	create_tray_icon();
 	Update(NULL); /* needed to kick it off */
 	g_timeout_add(interval, Update, NULL); /*update after 5sec*/
-	
 	gtk_main();
-
+	
 	return 0;
 }
 
